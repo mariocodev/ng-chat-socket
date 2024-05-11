0cbce439cf7b1ecd5f01da7bf34bece8de4e5c07
mariocodev <mariocodev@gmail.com>
2024-05-11 19:23:38 -0400

    feat: sendMessage from services
    Se configura service 'ChatService' para conexión con backend y envío de mensajes por defecto vía socket

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChatMessage } from 'src/app/models/chat-message.interface';
+import { ChatService } from 'src/app/services/chat.service';
 
 @Component({
-  selector: 'app-chat',
-  templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.scss']
+	selector: 'app-chat',
+	templateUrl: './chat.component.html',
+	styleUrls: ['./chat.component.scss']
 })
-export class ChatComponent {
+export class ChatComponent implements OnInit {
+
+	constructor(private chatService: ChatService){
+
+	}
+
+	ngOnInit(): void {
+		this.chatService.joinRoom("ABC");
+	}
+
+	sendMessage() {
+		const chatMessage = {
+			message: 'Hola',
+			user: '5'
+		} as ChatMessage
+		this.chatService.sendMessage("ABC", chatMessage);
+	}
+
+	
 
 }
--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { Stomp } from '@stomp/stompjs';
+import * as SockJS from 'sockjs-client';
+import { ChatMessage } from 'src/app/models/chat-message.interface';
 
 @Injectable({
-  providedIn: 'root'
+  	providedIn: 'root'
 })
 export class ChatService {
+	private stompClient: any;
 
-  constructor() { }
+	constructor(){
+		this.initConnectionSocket();
+	}
+
+	initConnectionSocket(){
+		const url = '//localhost:3000/chat-socket';
+		const socket = new SockJS(url);
+		this.stompClient = Stomp.over(socket);
+	}
+
+	joinRoom(roomId: string){
+		this.stompClient.connect({}, () => {
+			this.stompClient.subscribe(`/topic/${roomId}`, (messages: any) => {
+				const messageContent = JSON.parse(messages.body);
+				console.log(messageContent);
+			})
+		})
+	}
+
+	sendMessage(roomId: string, chatMessage: ChatMessage){
+		this.stompClient.send(`/app/chat/${roomId}`, {}, JSON.stringify(chatMessage));
+	}
 }
